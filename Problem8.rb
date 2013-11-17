340155c872c656cba3449978784ead04874959e9
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-17 00:15:37 -0800

    solved problem 8

--- Problem8.rb
+++ Problem8.rb
@@ -4,8 +4,26 @@
 # Sets n as the number as a string
 n = File.readlines("problem8_number.txt").map {|row| row = row.slice(0,50)}.join
 
+#test value
+# n = "73167176531330624919"
+
 #Create array of products
 
-#Find product of 5 numbers
+index = 0
+products = []
 
+while index < n.length
+	n.each_char do |char|
+	 product = n[index].to_i * n[index+1].to_i * n[index+2].to_i * n[index+3].to_i * n[index+4].to_i
+	 index = index + 1
+	 products << product
+	end
+end
+products.max
 
+# # Creates array containing each digit (as string) as an element 
+# array = []
+# n.each_char {|x| array.push x}
+# array
+
+# # chunk??
