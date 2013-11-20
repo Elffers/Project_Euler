0031169f7cea4d353f238872a1eb1c6bb421c51e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-20 11:19:42 -0800

    solved problem 9

--- Problem9.rb
+++ Problem9.rb
@@ -2,8 +2,7 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-#n = 993
-n = 20
+n = 1000
 range = (3..n)
 triples = []
 range.each do |a|
@@ -14,9 +13,12 @@ range.each do |a|
     end
   end
 end
+triples
 
-###
+match = triples.find_all do |triple|
+  triple[0]**2 + triple[1]**2 == triple[2]**2
+end
 
-
+match[0]*match[1]*match[2]
 
 
