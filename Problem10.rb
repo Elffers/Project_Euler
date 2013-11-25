a30b3b484c440add137b8db13c34faa1813d9e06
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-25 13:24:03 -0800

    solved problem 10 (long)

--- Problem10.rb
+++ Problem10.rb
@@ -5,17 +5,25 @@
 # Returns Array containing all primes below n
 # Tooo slow for large values of n
 
-n = 2000000
-primes = (2..n).to_a.select do |x|
-  (2..Math.sqrt(x)).to_a.select {|i| x % i == 0} == []
-end
+# n = 2000000
+# primes = (2..n).to_a.select do |x|
+#   (2..Math.sqrt(x)).to_a.select {|i| x % i == 0} == []
+# end
 
-n   = 10
+# n   = 100
+# sum = 0
+# primes = (2..n).to_a.select do |x|
+#   if (2..Math.sqrt(x)).select {|i| x % i == 0} == []
+#   	sum += x
+#   end
+# end
+
+# sum
+
+require 'prime'
+
 sum = 0
-primes = (2..n).to_a.select do |x|
-  if (2..Math.sqrt(x)).to_a.select {|i| x % i == 0} == []
-  	sum += x
-  end
-end
+n = 2000000
+primes = (2..n).find_all { |num| num < n && num.prime?}
+primes.inject{|sum, num| sum + num}
 
-sum
