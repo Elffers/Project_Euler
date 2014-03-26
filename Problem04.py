73fe162c244f8be46f4381698cacd442da23e190
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-26 12:05:01 -0700

    4 in python

---
+++ Problem04.py
@@ -0,0 +1,17 @@
+# Problem 4
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def palindrome(x):
+  return str(x) == str(x)[::-1]
+
+a = 999
+palindromes = []
+while a >= 100:
+  b = 999
+  while b >= 100:
+    if palindrome(a * b):
+      palindromes.append(a * b)
+    b -= 1
+  a -= 1
+
+max(palindromes)
