214caa6bf3d0e15f0426978ac27bf0f088e840e9
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-30 20:26:48 -0700

    prob 7 py

---
+++ python/Problem07.py
@@ -0,0 +1,16 @@
+# Problem 7
+# What is the 10 001st prime number?
+
+def nth_prime(n):
+    i = 2
+    primes = []
+    while(len(primes) < n):
+       j = 2
+       while(j <= (i/j)):
+          if not(i%j): break
+          j = j + 1
+       if (j > i/j) : primes.append(i)
+       i = i + 1
+    return primes.pop()
+
+# 104743
