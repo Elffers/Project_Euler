dca7a1ee9d7bd080a4b8300438536bbcbb213d25
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-30 20:19:36 -0700

    undo rename

--- python/Problem05.py
+++ python/Problem05.py
@@ -2,11 +2,14 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 def gcd(a, b):
-  remainder = b
-  other_number = a
-  while b:
-      a, remainder = b, a %
-  return a
+    """Calculate the Greatest Common Divisor of a and b.
+
+    Unless b==0, the result will have the same sign as b (so that when
+    b is divided by it, the result comes out positive).
+    """
+    while b:
+        a, b = b, a%b
+    return a
 
 def lcm(a, b):
   return a * b // gcd(a, b)
