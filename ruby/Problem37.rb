733c1b743ca01a20150a64a3c7785ac41a699c4a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-09-04 16:20:14 -0700

    solved 37 12 May 2014, 11pm

---
+++ ruby/Problem37.rb
@@ -0,0 +1,40 @@
+# The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+
+# Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+# NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+
+require 'prime'
+
+def truncatable?(prime)
+  return false if prime.to_s.length == 1
+  right_truncate(prime).all? {|x| x.prime? } && left_truncate(prime).all? {|x| x.prime? }
+end
+
+def right_truncate(num)
+  array = [num]
+  n = num.to_s.length - 1
+  n.times do
+    num = num.to_s.chop.to_i
+    array.push num
+  end
+  array
+end
+
+def left_truncate(num)
+  array = [num]
+  n = num.to_s.length - 1
+  n.times do
+    num = num.to_s.slice(1..-1).to_i
+    array.push num
+  end
+  array
+end
+
+def find_truncatable(n)
+  Prime.each(n).select { |prime| truncatable?(prime) }
+end
+
+p find_truncatable(1000000)
+ # => [23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 739397]
+
