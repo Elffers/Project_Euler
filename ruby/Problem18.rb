f2cd88ff624ba7f7bfc9dd596c0ad55068437ff3
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-09-08 21:20:43 -0700

    max_map works prb 18

--- ruby/Problem18.rb
+++ ruby/Problem18.rb
@@ -14,35 +14,31 @@ class Pathfinder
   attr_accessor :lines
 
   def initialize(input)
-    @lines = input.map { |line| line.chomp }.map { |nums| nums.split(/\s/).map { |x| x.to_i } }
+    @lines = input.map { |line| line.chomp }.map { |nums| nums.split(/\s/).map { |x| x.to_i } }.reverse
   end
 
-  # shift needs to happen before this is called
-  def next_max(max, index)
-    max = [@lines.first[index], @lines.first[index + 1]].max
-    index = index + 1 if @lines.first[index + 1] == max
-    return max, index
+  def map_max(line, index)
+    line.each_with_index.map do |num, i|
+      a = @lines[index - 1][i]
+      b = @lines[index - 1][i+1]
+      max = [a, b].max
+      max + num
+    end
   end
 
   def max_path
-    l = @lines.length
-    max = @lines.first.max
-    index = @lines.first.index(max)
-    path = []
-    l.times do
-      path << max
-      p path
-      @lines.shift
-      max, index = next_max(max, index) unless @lines.count == 0
+    @lines.each_with_index do |line, i|
+      @line[i] = map_max line, i
     end
-    path
   end
 end
 
-p = Pathfinder.new(File.readlines('problem18.txt'))
-path = p.max_path
-puts path.reduce(:+)
+if $0 == __FILE__
+  p = Pathfinder.new(File.readlines('problem18.txt'))
+  path = p.max_path
+  puts path.reduce(:+)
+  p = Pathfinder.new(File.readlines('big_triangle.txt'))
+  path = p.max_path
+  puts path.reduce(:+)
+end
 
-p = Pathfinder.new(File.readlines('big_triangle.txt'))
-path = p.max_path
-puts path.reduce(:+)
--- ruby/Problem18_spec.rb
+++ ruby/Problem18_spec.rb
@@ -7,37 +7,23 @@ describe Pathfinder do
 
   context 'initialize' do
     it 'sets array of arrays' do
-      expect(pathfinder.lines.first).to eq [3]
-      expect(pathfinder.lines.last).to eq [8, 5, 9, 3]
+      expect(pathfinder.lines.last).to eq [3]
+      expect(pathfinder.lines.first).to eq [2, 5, 9, 3]
     end
   end
 
-  context 'next_max' do
-    it 'finds the greater of two numbers in next array' do
-      pathfinder.lines.shift
-      next_max, index = pathfinder.next_max(3, 0)
-
-      expect(next_max).to eq 7
-      expect(index).to eq 0
-
-      pathfinder.lines.shift
-      next_max, index = pathfinder.next_max(7, 0)
-
-      expect(next_max).to eq 4
-      expect(index).to eq 1
-
-      pathfinder.lines.shift
-      next_max, index = pathfinder.next_max(4, 1)
-
-      expect(next_max).to eq 9
-      expect(index).to eq 2
+  context 'map_max' do
+    it 'maps each line to the max sum' do
+      index = 1
+      line = pathfinder.lines[index]
+      expect(pathfinder.map_max line, index).to eq [10, 13, 15]
     end
 
   end
 
   context 'max_path' do
-    it 'returns max' do
-      expect(pathfinder.max_path).to eq [3, 7, 4, 9]
+    xit 'returns max' do
+      expect(pathfinder.max_path).to eq 22
     end
   end
 
