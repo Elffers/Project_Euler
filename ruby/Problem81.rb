93727f7952b16f0d42bd5981f93d662066603369
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-10-17 20:33:50 -0700

    Problem 81 not quite working

--- ruby/Problem81.rb
+++ ruby/Problem81.rb
@@ -1,41 +1,44 @@
 # Find the minimal path sum, in matrix.txt, a 31K text file containing a 80 by 80 matrix, from the top left to
 # the bottom right by only moving right and down.
 
+class Pathfinder
+  attr_accessor :lines
 # Sanitize and format input into array of array of ints
 
-raw_matrix = File.readlines("matrix_81.txt")
-MATRIX = raw_matrix.map do |line|
-  line.delete("\n")
-  string = line.split(",")
-  string.map { |num| num.to_i }
+  def initialize(input)
+    @lines = input.map { |line| line.chomp }.map { |nums| nums.split(",").map { |x| x.to_i } }
+  end
+
+  def min_path(i, j)
+    if i == 0 && j == 0
+      return [@lines[i][j], -1 ,-1]
+    end
+    sum = @lines[i][j]
+    up = @lines[i-1][j]
+    left = @lines[i][j-1]
+    output = sum + up < sum + left ? [sum + up,i-1, j] : [sum + left, i, j-1]
+    @lines[output[1]][output[2]] = output[0]
+    output
+  end
+
+  def find_path
+    sums =# intialize grid 
+    i = lines.length - 1
+    j = lines.length - 1
+    current = @lines[i][j]
+    while i >= 0 && j >= 0
+      current, i, j = min_path i,j
+      p current
+    end
+    current
+  end
 end
 
-def choose_path(i, j)
-  if i == 79 && j == 79
-    return
-  end
-  if i == 79
-    return MATRIX[i][j+1], i, j+1
-  end
-  if j == 79
-    return MATRIX[i+1][j], i+1, j
-  end
-  right = MATRIX[i][j+1]
-  down = MATRIX[i+1][j]
-  right < down ? [right, i, j+1] : [down, i+1, j]
+if $0 === __FILE__
+  input = File.readlines "matrix81.txt"
+  pathfinder = Pathfinder.new input
+  pathfinder.lines.each {|x| p x }
+  p pathfinder.find_path
 end
 
-def find_path
-  sum = MATRIX[0][0]
-  i = 0
-  j = 0
-  while choose_path(i, j)
-    path, i, j = choose_path(i, j)
-    sum += path
-    choose_path(i,j)
-  end
-  sum
-end
-
-p find_path
-
+#should be 427337, getting 491866 
--- ruby/Problem81_spec.rb
+++ ruby/Problem81_spec.rb
@@ -1,21 +1,39 @@
 require_relative 'Problem81'
 
+describe Pathfinder do 
+  let(:input){ File.readlines "problem81_test.txt" }
+  let(:pf){ Pathfinder.new input }
+  let(:input2){ File.readlines "input81.txt" }
+  let(:pf2) { Pathfinder.new input2 }
 
-context 'choose_path' do
-  it 'chooses correct path' do
-    path = choose_path(0,0)
-    expect(path).to eq [1096, 1, 0]
+  context 'initialize' do
+    it 'creates array of arays' do
+      expect(pf.lines.first.first).to eq 131
+      expect(pf.lines.first.last).to eq 18
+      expect(pf.lines.last.last).to eq 331
+      expect(pf2.lines.first.first).to eq 1
+      expect(pf2.lines.last.last).to eq 9
+    end
   end
-  it 'only goes right if at the bottom line' do
-    path = choose_path(79, 0)
-    expect(path).to eq [5499, 79, 1]
+
+  context 'min_path' do
+    it 'returns min sum and coords' do
+      i = pf.lines.length - 1
+      j = pf.lines.first.length - 1
+      output  = pf.min_path(i,j)
+      expect(output).to eq [368, 4, 3]
+      a = 1
+      b = 4
+      sum, a, b  = pf.min_path(a,b)
+      output = pf.min_path(a,b)
+      expect(output).to eq [271, 0, 3]
+    end
   end
-  it 'only goes down if at the end of row' do
-    path = choose_path(0, 79)
-    expect(path).to eq [9377, 1, 79]
-  end
-  it 'stops if at last entry' do
-    path = choose_path(79, 79)
-    expect(path).to eq nil
+  
+  context 'find_path' do
+    it 'finds minimal path' do
+      expect(pf.find_path).to eq 2427
+      expect(pf2.find_path).to eq 21
+    end
   end
 end
